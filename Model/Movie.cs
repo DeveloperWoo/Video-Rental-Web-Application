a5744ab9945f6a1576977be2e328c290be63d671
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-01-30 14:32:34 -0500

    Add files via upload

--- Model/Movie.cs
+++ Model/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,17 @@ namespace VideoRental.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-    }
 
-    // /Movies/random
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        public Byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte NumberInStock { get; set; }
+    }
 }
