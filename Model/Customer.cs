46bdd4a3e51f71b31963cc4d8095eb45e1040dbd
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-02-01 00:09:36 -0500

    Add files via upload
    change 'label' name

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -9,11 +9,18 @@ namespace VideoRental.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
         public byte MembershipTypeId { get; set; } //foreign key
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
     }
 }
