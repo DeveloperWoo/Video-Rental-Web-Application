a18a00b3db67b47dcf79ee6828806da5beeb8ed0
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-02-01 00:53:32 -0500

    Add files via upload
    save Data

--- Controller/CustomersController.cs
+++ Controller/CustomersController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VideoRental.Models;
+using VideoRental.ViewModels;
+
 
 namespace VideoRental.Controllers
 {
@@ -25,8 +27,24 @@ namespace VideoRental.Controllers
 
         public ActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
