0c77aff3ae63b4c2c511468c7843b6c2b32333b7
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-02-02 18:17:08 -0500

    Add files via upload
    auto mapping

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideoRental.Dtos;
+using VideoRental.Models;
+
+namespace VideoRental.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {   
+            //Mapper.CreateMap<Source, Target>();
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
