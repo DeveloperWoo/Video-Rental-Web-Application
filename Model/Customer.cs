0f6aa7d0a7b1c0d242ac6cee58f1fa9aafd4262e
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-02-01 23:01:41 -0500

    Add files via upload
    override error msg (validation)

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -10,7 +10,7 @@ namespace VideoRental.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
